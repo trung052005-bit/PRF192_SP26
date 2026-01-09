79e8369e9719d7dc9691e3384cca903006e1f0b3
unknown <trung052005@gmail.com>
2026-01-09 11:21:51 +0700

    bai1

---
+++ slot2/bai1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+    /* Khai báo m?t bi?n s? nguyên,
+       kh?i t?o giá tr? ban d?u = 5 */
+    int a = 5;
+
+    // In giá tr? c?a a ra màn hình
+    printf("a=%2d \n", a);
+
+    // Gán giá tr? m?i cho a
+    a = 10;
+
+    // In giá tr? m?i c?a a
+    printf("a=%d", a);
+
+    return 0;
+}
