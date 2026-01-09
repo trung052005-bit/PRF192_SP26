37ecadc7253d736a606d679bc664945737b6601d
unknown <trung052005@gmail.com>
2026-01-09 11:05:41 +0700

    bai3

---
+++ slot2/bai3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+	// khai bao 1 so nguyen 
+	int S; //  giai thich: khai bao bien S là gia tri so nguyen 
+	
+	// khoi tao gia tri bat ky 
+	S = 10; //giai thich: gan gia tri ho bien S là 10 
+	
+	// in ra man hinh 
+	printf ("gia tri la: %d\n", S); // gia thich: in ra man hinh bảng lenh printf , và %d là chuoi dinh dang de gia tri dat vao dung vi tri do 
+	
+	return 0;
+}
